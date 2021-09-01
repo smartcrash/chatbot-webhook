83a76c014e5e410ebca9547198583282b1e49d62
Diego Da'Silva <smartcrashomg@gmail.com>
2021-09-01 12:42:40 -0400

    Format handler.js

--- handler.js
+++ handler.js
@@ -1,16 +1,12 @@
-"use strict";
-const fetch = require('node-fetch');
-
+'use strict'
 const fetch = require('node-fetch')
-
-const baseUrl = 'https://api.stagecliengo.com'
 
 /**
  * Documentación de request y response para chatbot webhooks
  * https://developers.cliengo.com/docs/new-message-webhook#response-json-example
- * 
- */ 
-module.exports.chatbotWebhook = async (event) => {
+ *
+ */
+module.exports.chatbotWebhook = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -32,49 +28,51 @@ module.exports.chatbotWebhook = async (event) => {
           "thing2": "something"
         },
         */
-        "response": {
-            "text": ["La hora es " + new Date().toLocaleString("en-US")],
-            "response_type": 'LIST',
-            "response_options": ["Model A", "Model B"],
-            "stopChat": true,
-            //"flow": 5
-        }
+        response: {
+          text: ['La hora es ' + new Date().toLocaleString('en-US')],
+          response_type: 'LIST',
+          response_options: ['Model A', 'Model B'],
+          stopChat: true,
+          //"flow": 5
+        },
       },
       null,
       2
     ),
-  };
-};
-
+  }
+}
 
 /**
  * Endpoint para simplificar la configuración de global_fulfillment_url que es la url donde se recibirán los webhooks de un determinado chatbot
- * 
- */ 
-module.exports.chatbotConfig = async (event) => {
-
-  const API_KEY = "d53ab56d-7b4a-491b-b8c3-41e260e991f1"
-  const websiteId = process.env.WEBSITE_ID || "60e5b8c52c6d8d0026157734" 
+ *
+ */
+module.exports.chatbotConfig = async event => {
+  const API_KEY = 'd53ab56d-7b4a-491b-b8c3-41e260e991f1'
+  const websiteId = process.env.WEBSITE_ID || '60e5b8c52c6d8d0026157734'
   const global_fulfillment_url = 'https://cae1-152-168-79-85.ngrok.io/dev/chatbotWebhook'
-  const baseUrl = "https://api.stagecliengo.com" // "https://api.cliengo.com"
+  const baseUrl = 'https://api.stagecliengo.com' // "https://api.cliengo.com"
 
   try {
-    var jwt = ""
+    var jwt = ''
     //busco un JWT para este user represetado por la API_KEY
-    await fetch(baseUrl+"/1.0/jwt?api_key="+API_KEY)
-    .then(res => res.json())
-    .then(json => { jwt = json.jwt })
+    await fetch(baseUrl + '/1.0/jwt?api_key=' + API_KEY)
+      .then(res => res.json())
+      .then(json => {
+        jwt = json.jwt
+      })
 
     //cambio el global_fulfillment_url para seterle el webhook de esta app
-    await fetch(baseUrl+"/1.0/projects/"+websiteId+"/chatbots/chatId", {
-      headers: {cookie: 'jwt='+jwt, 'Content-Type': 'application/json'},
+    await fetch(baseUrl + '/1.0/projects/' + websiteId + '/chatbots/chatId', {
+      headers: { cookie: 'jwt=' + jwt, 'Content-Type': 'application/json' },
       method: 'PATCH',
       body: JSON.stringify({
-        global_fulfillment_url :  global_fulfillment_url
-      })
+        global_fulfillment_url: global_fulfillment_url,
+      }),
     })
-    .then(res => res.json())
-    .then(json => { jwt = json })
+      .then(res => res.json())
+      .then(json => {
+        jwt = json
+      })
 
     //devuelvo el nuevo estado de la config de chatbot
     return {
@@ -86,23 +84,21 @@ module.exports.chatbotConfig = async (event) => {
         null,
         2
       ),
-    };
-    
+    }
   } catch (error) {
     console.log(error)
   }
+}
 
-};
-
-module.exports.hello = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Hello Cliengo world!',
-        },
-        null,
-        2
-      ),
-    };
-};
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hello Cliengo world!',
+      },
+      null,
+      2
+    ),
+  }
+}
